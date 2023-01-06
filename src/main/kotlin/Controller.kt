04a3b65a74930ea4194e66f258672646269f5cb4
Jia Hu <jia.hu0918@gmail.com>
2023-01-06 00:13:46 -0500

    Update Controller.kt

--- src/main/kotlin/Controller.kt
+++ src/main/kotlin/Controller.kt
@@ -1,1 +1,30 @@
+class Controller(var model: Model) {
 
+    fun initAlternating() {
+        model.initAlternating()
+    }
+
+    fun switchDataSet(index: Int) {
+        model.switchDataSet(index)
+    }
+
+    fun switchVisualization(index: Int) {
+        model.switchVisualization(index)
+    }
+
+    fun addEntry() {
+        model.addEntry()
+    }
+
+    fun removeEntry(index: Int) {
+        model.removeEntry(index)
+    }
+
+    fun createDataSet(dataSetName: String) {
+        model.createDataSet(dataSetName)
+    }
+
+    fun changeEntryValue(index: Int, newValue: Double) {
+        model.changeEntryValue(index, newValue)
+    }
+}
